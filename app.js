fdc79ff26950efe7380af21df375bcf62201577e
max-powell <github email address>
2019-08-20 12:57:30 +0100

    added yargs commands for add, remove, list, read with dummy text

--- app.js
+++ app.js
@@ -1,12 +1,53 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
+
 const getNotes = require('./notes')
 
-const command = process.argv[2]
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Title of new note',
+      demandOption: true,
+      type: 'string',
+      alias: 't'
+    },
+    body: {
+      describe: 'Text of note',
+      demandOption: true,
+      type: 'string',
+      alias: 'b'
+    }
+  },
+  handler: function(argv) {
+    console.log('Title: ' + argv.title)
+    console.log('Body: ' + argv.title)
+  }
+})
 
-switch (command) {
-  case 'add':
-    console.log('Adding note')
+yargs.command({
+  command: 'remove',
+  describe: 'Removes a note',
+  handler: function() {
+    console.log('Removing note')
+  }
+})
 
-  default:
-    console.log('Unknown command')
-}
+yargs.command({
+  command: 'list',
+  describe: 'Lists all notes',
+  handler: function() {
+    console.log('Listing notes')
+  }
+})
+
+yargs.command({
+  command: 'read',
+  describe: 'Reads a note',
+  handler: function () {
+    console.log('Reading note')
+  }
+})
+
+yargs.parse()
